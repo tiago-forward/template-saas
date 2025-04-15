028152389eaf645698b57c90fc45af04cf4b2c20
tiago-forward <devtiagoforward@gmail.com>
2025-04-15 15:37:57 -0300

    configured the database

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,9 +1,11 @@
+import { FirestoreAdapter } from "@auth/firebase-adapter";
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import { firebaseCert } from "./firebase";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
-  callbacks: {
-    
-  }
+  adapter: FirestoreAdapter({
+    credential: firebaseCert,
+  }),
 });
