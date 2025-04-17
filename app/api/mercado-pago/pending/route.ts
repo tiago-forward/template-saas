e90de76927a76ace4e11970c2147b0be20722707
tiago-forward <devtiagoforward@gmail.com>
2025-04-16 21:32:56 -0300

    added route pending mercado-pago

---
+++ app/api/mercado-pago/pending/route.ts
@@ -0,0 +1,31 @@
+import { NextRequest, NextResponse } from "next/server";
+import { Payment } from "mercadopago";
+import mpClient from "@/app/lib/mercado-pago";
+
+export async function GET(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
+  const paymentId = searchParams.get("payment_id");
+
+  const testeId = searchParams.get("external_reference");
+
+  if (!paymentId || !testeId) {
+    return NextResponse.json(
+      {
+        error: "Payment ID or teste ID not found",
+      },
+      { status: 400 }
+    );
+  }
+
+  const payment = new Payment(mpClient);
+
+  const paymentData = await payment.get({
+    id: paymentId,
+  });
+
+  if (paymentData.status === "approved" || paymentData.date_approved !== null) {
+    return NextResponse.json(new URL(`success`, req.url));
+  }
+
+  return NextResponse.json(new URL(`/failure`, req.url));
+}
