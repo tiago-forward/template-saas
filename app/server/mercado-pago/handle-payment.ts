6099ac5833cd88f977e7b6a5bfb57afba1ad87fd
tiago-forward <devtiagoforward@gmail.com>
2025-04-17 16:46:41 -0300

    fixed build problems

--- app/server/mercado-pago/handle-payment.ts
+++ app/server/mercado-pago/handle-payment.ts
@@ -4,5 +4,5 @@ export async function handleMercadoPagoPayment(paymentData: PaymentResponse) {
   const metadata = paymentData.metadata;
   const userEmail = metadata.user_email;
   const testeId = metadata.teste_id;
-  console.log("PAGAMENTO COM SUCESSO", paymentData);
+  console.log("PAGAMENTO COM SUCESSO", { paymentData, testeId, userEmail });
 }
